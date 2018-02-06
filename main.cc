f49c7f687c267dfdf4cdb0e96cf97838d4eeea7e
Keenan Burnett <keenburn2004@gmail.com>
2018-02-06 11:33:51 -0500

    vec file instead of image paths
    Now using a vec file instead of taking in the path to images.

--- main.cc
+++ main.cc
@@ -11,6 +11,7 @@
 #include <sensor_msgs/image_encodings.h>
 
 #include "Thirdparty/DLib/FileFunctions.h"
+#include <fstream>
 
 
 using namespace std;
@@ -19,44 +20,40 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "BagFromImages");
 
-    if(argc!=5)
+    if(argc!=4)
     {
-        cerr << "Usage: rosrun BagFromImages BagFromImages <path to image directory> <image extension .ext> <frequency> <path to output bag>" << endl;
+        cerr << "Usage: rosrun BagFromImages BagFromImages <path to .dat file> <frequency> <path to output bag>" << endl;
         return 0;
     }
 
     ros::start();
-
-    // Vector of paths to image
-    vector<string> filenames =
-            DUtils::FileFunctions::Dir(argv[1], argv[2], true);
-
-    cout << "Images: " << filenames.size() << endl;
-
+    std::string vFile(argv[1]);
+    std::ifstream infile(vFile, std::ifstream::in);
     // Frequency
-    double freq = atof(argv[3]);
-
+    double freq = atof(argv[2]);
     // Output bag
-    rosbag::Bag bag_out(argv[4],rosbag::bagmode::Write);
-
+    rosbag::Bag bag_out(argv[3],rosbag::bagmode::Write);
     ros::Time t = ros::TIME_MIN;
 
     const float T=1.0f/freq;
     ros::Duration d(T);
 
-    for(size_t i=0;i<filenames.size();i++)
+    int i = 0;
+    std::string file_name;
+    int x,y,w,h,dummy;
+    while(infile >> file_name >> dummy >> x >> y >> w >> h)
     {
         if(!ros::ok())
             break;
-
-        cv::Mat im = cv::imread(filenames[i],CV_LOAD_IMAGE_COLOR);
+        cv::Mat im = cv::imread(file_name,CV_LOAD_IMAGE_COLOR);
         cv_bridge::CvImage cvImage;
         cvImage.image = im;
-        cvImage.encoding = sensor_msgs::image_encodings::RGB8;
+        cvImage.encoding = sensor_msgs::image_encodings::BGR8;
         cvImage.header.stamp = t;
-        bag_out.write("/camera/image_raw",ros::Time(t),cvImage.toImageMsg());
+        bag_out.write("/camera/image_color",ros::Time(t),cvImage.toImageMsg());
         t+=d;
-        cout << i << " / " << filenames.size() << endl;
+        cout << i << endl;
+        i++;
     }
 
     bag_out.close();
